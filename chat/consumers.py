914b0069dba27858cdf9e27ae3333a6c370b5f87
Hardik Patel <hnmpatel@live.com>
2020-08-17 19:16:38 +0530

    Channel layer is integrated with redis server

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,20 +1,32 @@
 from channels.consumer import SyncConsumer
+from asgiref.sync import async_to_sync
 
 class EchoConsumer(SyncConsumer):
     def websocket_connect(self, event):
-        print("Connect event is called")
-
+        self.room_name = 'broadcast'
         self.send({
             'type': 'websocket.accept'
         })
+        async_to_sync(self.channel_layer.group_add)(self.room_name, self.channel_name)
+        print(f'[{self.channel_name}] - You are connected')
 
     def websocket_receive(self, event):
-        print(event)
+        print(f'[{self.channel_name}] - Recieved message - {event["text"]}')
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_name,
+             {
+                'type': 'websocket.message',
+                'text': event.get('text')
+             }
+        )
+
+    def websocket_message(self, event):
+        print(f'[{self.channel_name}] - Message sent - {event["text"]}')
         self.send({
             'type': 'websocket.send',
             'text': event.get('text')
         })
 
     def websocket_disconnect(self, event):
-        print("Connection is disconected")
-        print(event)
+        print(f'[{self.channel_name}] - Disonnected')
+        async_to_sync(self.channel_layer.group_discard)(self.room_name, self.channel_name)
--- chat_demo/settings.py
+++ chat_demo/settings.py
@@ -123,3 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
